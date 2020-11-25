d21cd9d8eb893f931a3ad352a5bd9fb862ff634e
Bernhard Hörmann <hoermann.b@gmail.com>
2020-11-25 12:17:15 +0100

    Add gap to the monitor tables

--- resources/views/livewire/dashboard.blade.php
+++ resources/views/livewire/dashboard.blade.php
@@ -3,7 +3,7 @@
         <livewire:server-monitor-run slot="Run all checks" />
     </div>
 
-    <div class="md:grid md:grid-cols-1">
+    <div class="md:grid md:grid-cols-1 gap-6">
         @foreach($hosts as $host)
             <livewire:server-monitor-table :host="$host"></livewire:server-monitor-table>
         @endforeach
